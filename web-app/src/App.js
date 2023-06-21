2313da9965e64f944356db034eb186d1f97f4cae
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-21 17:37:28 +0800

    Followed component file naming convention

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import HomePage from './pages/home'
+import HomePage from './pages/Home'
 
 import './styles/reset.scss'
 import './styles/main.scss'
