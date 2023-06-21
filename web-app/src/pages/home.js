ad08e906775a6b88fb872edca2aa50ff768aa1f0
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-21 17:31:29 +0800

    Added homepage styling and assets

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import logo from '../images/zeniark-logo.png'
+
 export default function HomePage() {
   return (
     <div className="page page-home">
+      <div className="site-logo">
+        <img src={logo} alt="zeniark logo" />
+      </div>
       <div className="page-title">
         <h1>Welcome to the Trivia Challenge!</h1>
       </div>
       <div className="page-body">
-        <p>You will be presented with 10 True or False questions.</p>
-        <p>Can you score 10/10?</p>
+        <p className="info">You will be presented with 10 True or False questions.</p>
+        <p className="question">Can you score 10/10?</p>
       </div>
       <div className="page-control">
         <button>
