b85baaf0fcc231788234b379155f873ca16f9b3d
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-22 13:58:29 +0800

    Removed unnecessary data structures

--- web-app/src/util/interfaces.ts
+++ web-app/src/util/interfaces.ts
@@ -18,32 +18,3 @@ export interface IState {
     score: number;
     currentIndex: number;
 }
-
-export interface IQuestionsAction {
-    type: "UPDATE_QUESTIONS";
-    questions: IQuestion[];
-}
-
-export interface IUpdateUserAnswerAction {
-    type: "UPDATE_USER_ANSWERS";
-    answer: IUserAnswer;
-}
-
-export interface IResetUserAnswerAction { type: "RESET_USER_ANSWERS"; }
-
-export interface IUpdateScoreAction {
-    type: "UPDATE_USER_SCORE";
-    score: number;
-}
-
-export interface IResetScoreAction {
-    type: "RESET_USER_SCORE";
-}
-
-export interface IUpdateCurrentIndexAction {
-    type: "UPDATE_CURRENT_QUESTION_INDEX";
-}
-
-export interface IResetCurrentIndexAction {
-    type: "RESET_CURRENT_QUESTION_INDEX";
-}
