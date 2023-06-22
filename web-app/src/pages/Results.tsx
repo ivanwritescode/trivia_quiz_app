45bf5f306975c3786c3d56a3a11f4a1a90a6440e
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-22 11:34:06 +0800

    Added defensive checks on Results component

--- web-app/src/pages/Results.tsx
+++ web-app/src/pages/Results.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from '../images/logo.png'
 import Card from "../UI/Card";
 import CardHeader from "../UI/CardHeader";
@@ -8,6 +8,7 @@ import he from 'he';
 import CardFooter from "../UI/CardFooter";
 import PageControl from "../components/PageControl";
 import { IQuestion, IUserAnswer } from "../util/interfaces";
+import { useNavigate } from "react-router-dom";
 
 interface IFinalResultsPageProps {
     questions: IQuestion[];
@@ -26,8 +27,18 @@ const getAnswerText = (answer: string) => {
 };
 
 const FinalResultsPage = ({ questions, answers, score, onClick }: IFinalResultsPageProps) => {
-    const getIsCorrect = (index: number) =>
-        questions[index].correct_answer === answers[index].value;
+    const navigate = useNavigate();
+
+    const getIsCorrect = (index: number) => {
+        if (answers.length === 0)
+            return;
+        return questions[index].correct_answer === answers[index].value;
+    }
+
+    useEffect(() => {
+        if(answers.length === 0)
+            navigate("/quiz")
+    });
     
     return (
         <main>
@@ -47,7 +58,7 @@ const FinalResultsPage = ({ questions, answers, score, onClick }: IFinalResultsP
                                 <div className="list-item-wrapper">
                                     <div className="list-group-text">
                                         <p className="question">{he.decode(item.question)}</p>
-                                        <p className="annotation">The correct answer is <b>{getAnswerText(item.correct_answer)}</b>. You answered {getAnswerText(answers[i].value)}</p>
+                                        <p className="annotation">The correct answer is <b>{getAnswerText(item.correct_answer)}</b>. You answered {answers.length > 0 && getAnswerText(answers[i].value)}</p>
                                     </div>
                                     <div className="list-group-icon">
                                         {getIsCorrect(i) ? <SuccessIcon /> : <FailedIcon />} 
